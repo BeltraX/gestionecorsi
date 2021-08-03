314b5d33ecf9aaf57c9ea2b2a49106d59ecf08c7
Betacom <Betacom@Betacm-PC9316.homenet.telecomitalia.it>
2021-08-03 14:23:57 +0200

    DAOConstants

--- gestionecorsi/src/main/java/com/milano/architecture/dao/DAOConstants.java
+++ gestionecorsi/src/main/java/com/milano/architecture/dao/DAOConstants.java
@@ -1,5 +1,25 @@
 package com.milano.architecture.dao;
 
 public interface DAOConstants {
+	String SELECT_CORSISTI = "select * from corsista";
+	String SELECT_DOCENTI = "select * from docente";
+	String SELECT_CORSI = "select * from corso";
+	String SELECT_ADMIN = "select * from amministratore";
+
+	String SELECT_CORSISTASEQ = "select corsista_seq.nextval from dual";
+	String SELECT_CORSOSEQ = "select corso_seq.nextval from dual";
+
+	String INSERT_CORSO = "insert into corso values(?,?,?,?,?,?,?,?)";
+	String INSERT_CORSISTA = "insert into corsista values(?,?,?,?)";
+	String COUNT_PARTECIP_BYID = "select count(*) from corso_corsista where codcorso = ?";
+	String SELECT_DURATA_GG_CORSO = "select datediff(day, datainiziocorso, datafinecorso) from corso where codcorso = ?";
+
+	String SELECT_DATA_ULTIMOCORSO = "select max(datainiziocorso) from corso";
+	String SELECT_TOT_CORSISTI = "select count(*) from corsista";
+	String COUNT_COMMENTI = "select count(commenticorso) from corso";
+
+	String DELETE_CORSO = "Delete from corso where codcorso = ?";
+	String DELETE_UTENTE = "Delete from utente where username = ?";
+	String DELETE_ARTICOLO = "Delete from articolo where id_articolo = ?";
 
 }
