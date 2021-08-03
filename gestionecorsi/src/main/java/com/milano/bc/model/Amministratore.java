0a8aecb4d5af9903642fb35d77613fd2d4912b32
BeltraX <andreabeltramin00@gmail.com>
2021-08-03 14:53:27 +0200

    Conclusi Model

---
+++ gestionecorsi/src/main/java/com/milano/bc/model/Amministratore.java
@@ -0,0 +1,26 @@
+package com.milano.bc.model;
+
+public class Amministratore {
+	private String nome;
+	private String cognome;
+	private int codice;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public int getCodice() {
+		return codice;
+	}
+	public void setCodice(int codice) {
+		this.codice = codice;
+	}
+}
---
+++ gestionecorsi/src/main/java/com/milano/bc/model/Corso.java
@@ -0,0 +1,56 @@
+package com.milano.bc.model;
+
+import java.util.Date;
+
+public class Corso {
+	private int codice;
+	private String nome;
+	private Date dataInizio;
+	private Date dataFine;
+	private double costo;
+	private String commento;
+	private String aula;
+	
+	public int getCodice() {
+		return codice;
+	}
+	public void setCodice(int codice) {
+		this.codice = codice;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Date getDataInizio() {
+		return dataInizio;
+	}
+	public void setDataInizio(Date dataInizio) {
+		this.dataInizio = dataInizio;
+	}
+	public Date getDataFine() {
+		return dataFine;
+	}
+	public void setDataFine(Date dataFine) {
+		this.dataFine = dataFine;
+	}
+	public double getCosto() {
+		return costo;
+	}
+	public void setCosto(double costo) {
+		this.costo = costo;
+	}
+	public String getCommento() {
+		return commento;
+	}
+	public void setCommento(String commento) {
+		this.commento = commento;
+	}
+	public String getAula() {
+		return aula;
+	}
+	public void setAula(String aula) {
+		this.aula = aula;
+	}
+}
