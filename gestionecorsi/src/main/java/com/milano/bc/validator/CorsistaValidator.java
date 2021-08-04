366c5fcdec54a471e0abf65b48a9d553300fedcf
Betacom <Betacom@Betacm-PC9316.homenet.telecomitalia.it>
2021-08-04 11:36:31 +0200

    validator e test validator

--- gestionecorsi/src/main/java/com/milano/bc/validator/CorsistaValidator.java
+++ gestionecorsi/src/main/java/com/milano/bc/validator/CorsistaValidator.java
@@ -1,6 +1,23 @@
 package com.milano.bc.validator;
 
-public class CorsistaValidator {
+import java.util.regex.Pattern;
 
-	
+import com.milano.bc.model.Corsista;
+
+public class CorsistaValidator {
+	public boolean checkNome(Corsista corsista) {
+		return Pattern.matches("^[a-zA-Z ,.'-]{2,30}+$", corsista.getNomeCorsista());
+	}
+
+	public boolean checkCognome(Corsista corsista) {
+		return Pattern.matches("^[a-zA-Z ,.'-]{2,30}+$", corsista.getCognomeCorsista());
+	}
+
+	public boolean checkPrecFormativi(Corsista corsista) {
+		int x = corsista.getPrecedentiformativi();
+		if (x == 0 || x == 1) {
+			return true;
+		} else
+			return false;
+	}
 }
