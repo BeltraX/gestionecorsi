7882dc4e99a53ab19cb330cb4fefab7821056e54
DaviGhast <davide.mainardi2001@hotmail.com>
2021-08-04 09:59:51 +0200

    modifica CorsoIdGeneratorTest

--- gestionecorsi/src/main/java/test/com/milano/bc/idgenerator/CorsistaIdGeneratorTest.java
+++ gestionecorsi/src/main/java/test/com/milano/bc/idgenerator/CorsistaIdGeneratorTest.java
@@ -1,5 +1,6 @@
 package test.com.milano.bc.idgenerator;
 
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -13,12 +14,16 @@ import com.milano.bc.idgenerator.CorsistaIdGenerator;
 class CorsistaIdGeneratorTest {
 
 	@Test
-	void testIdGenerator() throws IOException, ClassNotFoundException, SQLException{
-			CorsistaIdGenerator idGen =  CorsistaIdGenerator.getIstance();
+	void testIdGenerator() {
+		try {
+			CorsistaIdGenerator idGen = CorsistaIdGenerator.getIstance();
 			assertNotNull(idGen, "Istanza non creata tramite il singleton");
 			long valore = idGen.getNextId();
 			assertEquals(valore, idGen.getNextId() - 1);
-		
+		} catch (ClassNotFoundException | SQLException | IOException exc) {
+			exc.printStackTrace();
+			fail(exc.getMessage());
+		}
 	}
 
 }
